3825c0ca134206b1c16ba65c016d3699832264b5
NageshVagicharla <nagesh8421@gmail.com>
2021-05-05 01:39:30 -0400

    Added additional routes

---
+++ busdata3.py
@@ -0,0 +1,40 @@
+from pykafka import KafkaClient
+import json
+from datetime import datetime
+import uuid
+
+ipt_file = open('bus3.json')
+json_array = json.load(ipt_file)
+coords = json_array["features"][0]["geometry"]["coordinates"]
+
+def generate_uuid():
+    return uuid.uuid4()
+
+client = KafkaClient(hosts="localhost:9092")
+topic = client.topics['busdata']
+producer = topic.get_sync_producer()
+
+data = {}
+data["busline"] = '00003'
+
+def generate_ckeckpoint(coords):
+    i = 0
+    while(i < len(coords)):
+        data['key'] = data['busline'] + '_' + str(generate_uuid())
+        data['timestamp'] = str(datetime.utcnow())
+        data['latitude'] = coords[i][1]
+        data['longitude'] = coords[i][0]
+        
+        message = json.dumps(data)
+        producer.produce(message.encode('ascii'))
+        print(message)
+        i += 1
+
+generate_ckeckpoint(coords)
+
+
+
+
+
+
+# producer.produce('test message2'.encode('ascii'))
